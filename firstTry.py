156bd3c6ac6faade5151ef24eeb50db2f8017390
unknown <pauloduga3679@gmail.com>
2022-09-04 16:03:54 +0800

    Second Problem Solution

--- firstTry.py
+++ firstTry.py
@@ -1,23 +1,20 @@
-# Python Essentials Coding Challenges 1
-#
-#Create 4 Variables (Name (string),Math Grade (float),Science Grade (float),
-# 1.English Grade (integer),Status (string))
-# 2. Assign appropriate value
-# 3. Display the result:
-# Name: __________
-# Math Grade:_____________
-# Science Grade:___________
-# English Grade:___________ 
-#Status:______________
-
-
-Name="Paulo"
-MathGrade="Passed"
-ScienceGrade="Passed"
-EnglishGrade="Failed"
-
-print("-----Examiminations Final Results------")
-print("Name: ",Name)
-print("Math Grade: ",MathGrade)
-print("Science Grade: ",ScienceGrade)
-print("English Grade: ",EnglishGrade)
+#1.Create 4 Variables use input function() (Name (string),Math Grade
+#(float),Science Grade (float),English Grade (integer), Average (float))
+#2. Use input statement
+#3.Display the result:
+#Name: __________
+#Math Grade:_____________
+#Science Grade:___________
+#English Grade:___________ 
+#Average:______________
+print("\n")
+print("-----Get My Average App-----")
+print("\n")
+Name=input("Please Input you Name: ")
+MathGrade=float(input("Please Input your Math Grade: "))
+ScienceGrade=float(input("Please Input your Science Grade: "))
+EnglishGrade=float(input("Please Input your English Grade: "))
+average=(MathGrade+ScienceGrade+EnglishGrade)/4
+print("\n")
+print("Hello!! ",Name+"Your General Average is {:.2f}".format(average))
+print("\n")
