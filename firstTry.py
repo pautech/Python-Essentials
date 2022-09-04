8005fbf74b9052c8d54613ada37962647e2ecc66
unknown <pauloduga3679@gmail.com>
2022-09-04 15:05:17 +0800

    First Solution to the Problem

---
+++ firstTry.py
@@ -0,0 +1,23 @@
+# Python Essentials Coding Challenges 1
+#
+#Create 4 Variables (Name (string),Math Grade (float),Science Grade (float),
+# 1.English Grade (integer),Status (string))
+# 2. Assign appropriate value
+# 3. Display the result:
+# Name: __________
+# Math Grade:_____________
+# Science Grade:___________
+# English Grade:___________ 
+#Status:______________
+
+
+Name="Paulo"
+MathGrade="Passed"
+ScienceGrade="Passed"
+EnglishGrade="Failed"
+
+print("-----Examiminations Final Results------")
+print("Name: ",Name)
+print("Math Grade: ",MathGrade)
+print("Science Grade: ",ScienceGrade)
+print("English Grade: ",EnglishGrade)
